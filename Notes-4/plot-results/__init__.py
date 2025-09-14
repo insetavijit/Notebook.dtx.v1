ed55d6db65c76b3af237ad5b2c78fd5e5e854a83
Avijit Sarkar <avisoffice@gmail.com>
2025-09-14 13:45:03 +0530

    Auto-commit 2025-09-14 08:15:03 - Notes-4/plot-results/__init__.py, Notes-4/plot-results/config.py, Notes-4/plot-results/plotter.py, Notes-4/plot-results/utils.py, Notes-4/see-tst-results.ipynb

---
+++ Notes-4/plot-results/__init__.py
@@ -0,0 +1,3 @@
+from .plotter import CandlestickPlotter
+
+__all__ = ["CandlestickPlotter"]
---
+++ Notes-4/plot-results/config.py
@@ -0,0 +1,10 @@
+# Default configuration for plotting
+DEFAULT_PLOT_CONFIG = {
+    'figsize': (20, 8),
+    'style': 'charles',
+    'title': 'Candlestick Chart with Trade Rectangles',
+    'main_rect': {'fill_color': 'orange', 'edge_color': 'black', 'alpha': 0.1, 'linewidth': 1.5},
+    'sl_rect': {'fill_color': 'red', 'edge_color': 'black', 'alpha': 0.1, 'linewidth': 1.5},
+    'tp_rect': {'fill_color': 'green', 'edge_color': 'black', 'alpha': 0.1, 'linewidth': 1.5},
+    'golden_outline': {'edge_color': 'gold', 'linewidth': 4}
+}
